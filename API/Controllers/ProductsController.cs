862c622fb810f8f51870089149de28b037c932a1
igorflyunt <igorflyunt2@gmail.com>
2021-09-28 00:39:35 +0300

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will return list of products";
+        }
+        
+        [HttpGet("id")]
+        public string GetProduct(int id)
+        {
+            return "return single  product";
+        }
+    }
+}
