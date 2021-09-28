a9598f61ce4f6b76d876ef8442bd7fe6a70f593e
igorflyunt <igorflyunt2@gmail.com>
2021-09-28 23:52:49 +0300

    move data and model to separate project, add and change all dependencies

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,7 +24,7 @@ namespace API.Controllers
             var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
-        
+
         [HttpGet("id")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,11 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -23,20 +16,14 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-           
         }
 
-      
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }); });
 
             services.AddDbContext<StoreContext>(builder => builder.UseSqlite(_config.GetConnectionString(
                 "DefaultConnection")));
@@ -58,10 +45,7 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
